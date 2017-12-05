3c36c696848d4a1a8f65c7b9a0600b77ede707d6
osolomin90 <oleksandr.solomin@perfectial.com>
2017-12-05 17:28:36 +0200

    update agent

--- locust_process_monitor_agent.py
+++ locust_process_monitor_agent.py
@@ -9,12 +9,13 @@ def _get_cpu_and_memory():
 
 class LocustProcessMonitorAgent(object):
 
-    def __init__(self, port=9090):
+    def __init__(self, port=9999):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(('', port))
         self.conn = None
 
     def wait_client(self):
+        print "agent wait for client connection"
         self.sock.listen(1)
         self.conn, addr = self.sock.accept()
         print 'connected:', addr
@@ -22,15 +23,24 @@ class LocustProcessMonitorAgent(object):
     def launch_process_monitor_agent(self):
         while True:
             self.wait_client()
-            data = self.conn.recv(1024)
-            if data == 'locust client':
-                try:
-                    while True:
+            try:
+                while True:
+                    data = self.conn.recv(1024)
+                    if data == 'locust client':
                         self.conn.send(pickle.dumps(_get_cpu_and_memory()))
                         print "send"
-                except Exception:
-                    print "can not send charts!!!"
-                    self.close_connection()
+                    elif data == 'done':
+                        self.close_connection()
+                        print "client closed connection"
+                        break
+                    else:
+                        self.close_connection()
+                        print "connection is close"
+                        break
+            except Exception:
+                print "can not send charts!!!"
+            finally:
+                self.close_connection()
 
     def close_connection(self):
         self.conn.close()
